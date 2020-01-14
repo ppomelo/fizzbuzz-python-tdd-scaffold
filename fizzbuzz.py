e6ce8a4488681f04761b0fec2d393a427848a51e
ppomelo <327881179@qq.com>
2020-01-15 00:12:14 +0800

    fix py style

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-def FizzBuzz(num):
+def fizz_buzz(num):
 
     if num % 3 == 0 and num % 5 == 0:
         return 'FizzBuzz'
@@ -8,9 +8,10 @@ def FizzBuzz(num):
 
     if num % 5 == 0:
         return 'Buzz'
-    
+
     return str(num)
+
 
 if __name__ == '__main__':
     for i in range(1, 101):
-        print(FizzBuzz(i))
+        print(fizz_buzz(i))
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,16 +1,16 @@
 import unittest
-from fizzbuzz import FizzBuzz
+from fizzbuzz import fizz_buzz
 
 class test_fb(unittest.TestCase):
 
     def test_default_case(self):
-        self.assertEqual(FizzBuzz(1), '1')
+        self.assertEqual(fizz_buzz(1), '1')
 
     def test_fizz(self):
-        self.assertEqual(FizzBuzz(6), 'Fizz')
+        self.assertEqual(fizz_buzz(6), 'Fizz')
     
     def test_buzz(self):
-        self.assertEqual(FizzBuzz(10), 'Buzz')
+        self.assertEqual(fizz_buzz(10), 'Buzz')
 
     def test_fizzbuzz(self):
-        self.assertEqual(FizzBuzz(30), 'FizzBuzz')
+        self.assertEqual(fizz_buzz(30), 'FizzBuzz')
